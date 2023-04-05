460abb49b3c613f4a6a3eb24ac04fc1b8f215eb0
MatC-DB <57893067+MatC-DB@users.noreply.github.com>
2023-04-05 14:39:07 +0100

    refactor: view model split

---
+++ MainWindowViewModel.window.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Zephyr
+{
+    public partial class MainWindowViewModel : ReactiveObject, IScreen
+    {
+        public string Version { get; } = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "DEBUG";
+
+        [DataMember]
+        public double Width { get; set; } = 800;
+
+        [DataMember]
+        public double Height { get; set; } = 450;
+
+        public WindowState State { get; set; } = WindowState.Normal;
+
+        [DataMember]
+        public bool IsFullScreen
+        {
+            get => State == WindowState.Maximized;
+            set
+            {
+                if (value && State == WindowState.Normal) State = WindowState.Maximized;
+            }
+        }
+
+        public Avalonia.PixelPoint Position { get; set; } = new Avalonia.PixelPoint(50, 50);
+
+        [DataMember]
+        public int Top { get { return Position.Y; } set { Position = new Avalonia.PixelPoint(Position.X, value); } }
+
+        [DataMember]
+        public int Left { get { return Position.X; } set { Position = new Avalonia.PixelPoint(value, Position.Y); } }
+    }
+}
